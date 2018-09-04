cd2d1adffed35c45694d4c8bab0b5511623a179e
tomgiagtz <tom.giagtzoglou@gmail.com>
2018-09-04 17:12:54 -0400

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,6 +15,14 @@ def first_challenge
 
   #your code here
 
+ contacts.each do |name, person|
+ 	person.each do |key, value|
+ 		if key == :favorite_icecream_flavors
+ 			value.delete_if { |flavor|  flavor == "strawberry"}
+ 		end
+ 	end
+ end
+
 
   #remember to return your newly altered contacts hash!
   contacts
